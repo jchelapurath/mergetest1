f51b684b7d8aaecc1bd69498f51ccfca279e734f
jchelapurath <jchelapurath@student.tgm.ac.at>
2017-03-13 10:12:09 +0100

    push1

---
+++ src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+	
+	public static void gutentag (String  [] args) {
+		System.out.println("gutentag");
+		
+	}
+	
+	public static void byebye (String  [] args) {
+		System.out.println("byebye");
+		
+	}
+	
+	public static void main (String  [] args) {
+		gutentag();
+		byebye();
+		
+	}
+
+}
